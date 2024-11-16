0d4decee7e27cba2b99ffd0e49bda8bacbfd4941
Thrishangowda <thrishugowda2003@gmail.com>
2024-11-15 17:45:49 -0800

    Initial commit with  code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* Top Bar */}
+      <div className="top-bar">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          !! S J C Institute of technology | ✉ sjcit@gmail.com | 📞 123-456-789
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </div>
+
+      {/* Navbar */}
+      <nav className="navbar">
+        {/* <p1>&#x1F393;</p1> */}
+
+        <div className="logo">
+          Welcome to <p1>&#x1F393;</p1> HRD
+        </div>
+
+        {/* <ul className="nav-links">
+          <li>
+            <a href="#home">Jobs</a>
+          </li>
+          <li>
+            <a href="#dropdown">Profiles</a>
+          </li>
+          <li>
+            <a href="#courses">Updates</a>
+          </li>
+        </ul> */}
+      </nav>
+
+      {/* Header Section */}
+      <header className="header">
+        <div className="header-content">
+          {/* <h1>
+            <p1>&#x1F393;</p1>Welcome to Department of HRD
+          </h1>
+
+          <div className="header-buttons">
+            <button className="apply-btn">Visit Official Website</button>
+          </div> */}
+          <div className="nav-links">
+            <li>
+              <a href="#home">Jobs</a>
+            </li>
+            <li>
+              <a href="#dropdown">Profiles</a>
+            </li>
+            <li>
+              <a href="#courses">Updates</a>
+            </li>
+          </div>
+        </div>
       </header>
+
+      {/* Info Sections */}
+      {/* <div className="info-section">
+        <div className="info-box programs">
+          <h2>PROGRAMS</h2>
+        </div>
+        <div className="info-box affordability">
+          <h2>AFFORDABILITY</h2>
+        </div>
+        <div className="info-box certification">
+          <h2>CERTIFICATION</h2>
+        </div>
+      </div> */}
     </div>
   );
 }
