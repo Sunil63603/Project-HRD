6e16b5446cf4859ae692ca308a15432de20f47df
Up786141 <pashau293@gmail.com>
2024-12-04 10:06:45 +0530

    added the comments in the HR_Layout

--- src/HR_Page/HR_Layout/HR_Layout.jsx
+++ src/HR_Page/HR_Layout/HR_Layout.jsx
@@ -5,7 +5,13 @@ import { Outlet } from "react-router-dom"; // Outlet component is a placeholders
 const HR_Layout = () => {
   return (
     <>
-      <Navbar /> {/* Navbar is displayed always*/}
+      {/* Navbar is displayed always*/}
+      {/* The Navbar contains 4 child components,which ever component btn is clicked (in Navbar) that particular path is displayed in URL bar.*/}
+      {/* The URL bar triggers the Routes and that particular path component is displyed in the UI */}
+      <Navbar />
+      {/* The use of outlet is to trigger the child components */}
+      {/* The child components means the Route (component) contains the sub-Route (component)*/}
+      {/* In this we have the (HR_Layout) as the parent component and the (Navbar and 4 components)are the child components which is passed as the prop in the Outlet */}
       <Outlet />
     </>
   );
