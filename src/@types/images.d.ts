8409ceaeaae0ff59b0a6b606bc61322e7fc68175
Sunil63603 <s60667843@gmail.com>
2025-05-14 14:30:10 +0530

    cleared entire local sunil_Studentcode branch

--- src/@types/images.d.ts
+++
@@ -1,8 +0,0 @@
-///<reference types='react-scripts'/>
-
-//👇code for images import and export.
-declare module "*.png*";
-declare module "*.jpg*";
-declare module "*.jpeg*";
-declare module "*.gif*";
-declare module "*.svg";
--- src/config.ts
+++
@@ -1,4 +0,0 @@
-//because of this below export line,i was able to import CHATBOT_ID in index.html
-export const CHATBOT_ID = import.meta.env.VITE_CHATBOT_ID;
-
-// VITE_CHATBOT_ID type is defined in vite-env.d.ts file.
